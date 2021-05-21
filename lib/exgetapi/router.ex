0b7b1ae574e67b60a66d56442a04ad803d6f309f
Andre Kurniawan <drekunia@gmail.com>
2021-05-21 12:27:28 +0700

    comments

--- lib/exgetapi/router.ex
+++ lib/exgetapi/router.ex
@@ -15,34 +15,36 @@ defmodule Exgetapi.Router do
   plug(Plug.Parsers, parsers: [:json], json_decoder: Jason)
   plug(:dispatch)
 
+  # strip meta from Name in Jason.encode
   Protocol.derive(Jason.Encoder, Name, except: [:__meta__])
 
-  def init(options) do
-    IO.inspect(options)
-  end
-
+  # send json response
   defp respond(conn, code, payload) do
     conn
     |> Plug.Conn.put_resp_content_type("application/json")
     |> send_resp(code, payload)
   end
 
+  # send names record if success
   defp send(conn, code, names) when is_list(names) do
     payload = Jason.encode!(%{"code" => code, "data" => names})
 
     conn |> respond(code, payload)
   end
 
+  # send error message
   defp send(conn, code, message) when is_bitstring(message) do
     payload = Jason.encode!(%{"code" => code, "message" => message})
 
     conn |> respond(code, payload)
   end
 
+  # error message: invalid id (id must be a string of integer or integers separated by commas)
   defp invalid_id(conn, string_ids) do
     conn |> send(404, "invalid or empty ID: \"#{string_ids}\"")
   end
 
+  # error message: Name.id not exist
   defp not_exist_id(conn, string_ids) do
     conn |> send(404, "resource with ID #{string_ids} not exist")
   end
@@ -50,34 +52,42 @@ defmodule Exgetapi.Router do
   get "/" do
     map_params = Plug.Conn.fetch_query_params(conn).query_params
 
-    # check if there's id on query params
     if Map.has_key?(map_params, "id") do
       string_ids = Map.fetch!(map_params, "id")
-      list_ids = String.split(string_ids, ",", trim: true)
 
-      if list_ids != [] do
-        enum_ids =
-          Enum.flat_map(list_ids, fn string_id ->
-            if Regex.match?(~r{\A\d*\z}, string_id) do
-              {int_id, _} = Integer.parse(string_id)
-              [int_id]
+      # check if string_ids only contains digits separated by commas
+      if Regex.match?(~r{^\d+(,\d+)*$}, string_ids) do
+        list_ids = String.split(string_ids, ",", trim: true)
+
+        if list_ids != [] do
+          # parse strings in list_ids into integers
+          enum_ids =
+            Enum.flat_map(list_ids, fn string_id ->
+              case Integer.parse(string_id) do
+                # transform to integer
+                {int_id, _} -> [int_id]
+                # skip the value
+                :error -> []
+              end
+            end)
+
+          if enum_ids != [] do
+            # query Name if Name.id is in enum_ids
+            query =
+              from(n in Name,
+                join: i in fragment("unnest(?::int[])", ^enum_ids),
+                on: n.id == i,
+                select: n
+              )
+
+            # send result if exist
+            if Repo.all(query) != [] do
+              conn |> send(200, Repo.all(query))
             else
-              []
+              conn |> not_exist_id(string_ids)
             end
-          end)
-
-        if enum_ids != [] do
-          query =
-            from(n in Name,
-              join: i in fragment("unnest(?::int[])", ^enum_ids),
-              on: n.id == i,
-              select: n
-            )
-
-          if Repo.all(query) != [] do
-            conn |> send(200, Repo.all(query))
           else
-            conn |> not_exist_id(string_ids)
+            conn |> invalid_id(string_ids)
           end
         else
           conn |> invalid_id(string_ids)
@@ -86,6 +96,7 @@ defmodule Exgetapi.Router do
         conn |> invalid_id(string_ids)
       end
     else
+      # send all names when no id in_query params
       conn |> send(200, Repo.all(Name))
     end
   end
