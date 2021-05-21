b1877d5713dd45a15bd6b06ea618cf832a3b82ed
Andre Kurniawan <drekunia@gmail.com>
2021-05-21 14:08:10 +0700

    separate functions

--- lib/exgetapi/router.ex
+++ lib/exgetapi/router.ex
@@ -10,7 +10,6 @@ defmodule Exgetapi.Router do
   import Ecto.Query
 
   plug(Plug.Logger, log: :debug)
-  plug(Plug.Head)
   plug(:match)
   plug(Plug.Parsers, parsers: [:json], json_decoder: Jason)
   plug(:dispatch)
@@ -26,82 +25,88 @@ defmodule Exgetapi.Router do
   end
 
   # send names record if success
-  defp send(conn, code, names) when is_list(names) do
-    payload = Jason.encode!(%{"code" => code, "data" => names})
+  defp send_result(conn, names) when is_list(names) do
+    payload = Jason.encode!(%{"code" => 200, "data" => names})
 
-    conn |> respond(code, payload)
+    conn |> respond(200, payload)
   end
 
   # send error message
-  defp send(conn, code, message) when is_bitstring(message) do
-    payload = Jason.encode!(%{"code" => code, "message" => message})
+  defp send_result(conn, message) when is_bitstring(message) do
+    payload = Jason.encode!(%{"code" => 404, "message" => message})
 
-    conn |> respond(code, payload)
+    conn |> respond(400, payload)
   end
 
   # error message: invalid id (id must be a string of integer or integers separated by commas)
   defp invalid_id(conn, string_ids) do
-    conn |> send(404, "invalid or empty ID: \"#{string_ids}\"")
+    conn |> send_result("invalid or empty ID: \"#{string_ids}\"")
   end
 
   # error message: Name.id not exist
   defp not_exist_id(conn, string_ids) do
-    conn |> send(404, "resource with ID #{string_ids} not exist")
+    conn |> send_result("resource with ID #{string_ids} not exist")
+  end
+
+  defp query_enum_ids(conn, enum_ids) when enum_ids == [] do
+    conn |> invalid_id(Enum.join(enum_ids, ","))
+  end
+
+  defp query_enum_ids(conn, enum_ids) do
+    names =
+      Repo.all(
+        from(n in Name,
+          join: i in fragment("unnest(?::int[])", ^enum_ids),
+          on: n.id == i,
+          select: n
+        )
+      )
+
+    result(conn, enum_ids, names)
+  end
+
+  defp enum_list_ids(conn, list_ids) when list_ids == [] do
+    conn |> invalid_id(Enum.join(list_ids, ","))
+  end
+
+  defp enum_list_ids(conn, list_ids) do
+    enum_ids =
+      Enum.flat_map(list_ids, fn string_id ->
+        case Integer.parse(string_id) do
+          # transform to integer
+          {int_id, _} -> [int_id]
+          # skip the value
+          :error -> []
+        end
+      end)
+
+    query_enum_ids(conn, enum_ids)
+  end
+
+  defp result(conn, enum_ids, names) when names == [] do
+    conn |> not_exist_id(Enum.join(enum_ids, ","))
+  end
+
+  defp result(conn, _, names) do
+    conn |> send_result(names)
+  end
+
+  defp match_regex(conn, string_ids) do
+    if Regex.match?(~r{^\d+(,\d+)*$}, string_ids),
+      do: conn |> enum_list_ids(String.split(string_ids, ",", trim: true)),
+      else: conn |> invalid_id(string_ids)
   end
 
   get "/" do
     map_params = Plug.Conn.fetch_query_params(conn).query_params
 
-    if Map.has_key?(map_params, "id") do
-      string_ids = Map.fetch!(map_params, "id")
-
-      # check if string_ids only contains digits separated by commas
-      if Regex.match?(~r{^\d+(,\d+)*$}, string_ids) do
-        list_ids = String.split(string_ids, ",", trim: true)
-
-        if list_ids != [] do
-          # parse strings in list_ids into integers
-          enum_ids =
-            Enum.flat_map(list_ids, fn string_id ->
-              case Integer.parse(string_id) do
-                # transform to integer
-                {int_id, _} -> [int_id]
-                # skip the value
-                :error -> []
-              end
-            end)
-
-          if enum_ids != [] do
-            # query Name if Name.id is in enum_ids
-            query =
-              from(n in Name,
-                join: i in fragment("unnest(?::int[])", ^enum_ids),
-                on: n.id == i,
-                select: n
-              )
-
-            # send result if exist
-            if Repo.all(query) != [] do
-              conn |> send(200, Repo.all(query))
-            else
-              conn |> not_exist_id(string_ids)
-            end
-          else
-            conn |> invalid_id(string_ids)
-          end
-        else
-          conn |> invalid_id(string_ids)
-        end
-      else
-        conn |> invalid_id(string_ids)
-      end
-    else
-      # send all names when no id in_query params
-      conn |> send(200, Repo.all(Name))
-    end
+    # send all names when no id in_query params
+    if Map.has_key?(map_params, "id"),
+      do: conn |> match_regex(Map.fetch!(map_params, "id")),
+      else: conn |> send_result(Repo.all(Name))
   end
 
   match _ do
-    conn |> send(404, "Error 404 Not Found")
+    conn |> send_result("Error 404 Not Found")
   end
 end
